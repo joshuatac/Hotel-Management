d63abbbd62016c400278d99fcc8a298a93273190
Joshuatac <4surejosh@gmail.com>
2022-11-15 13:42:29 +0100

    register user functionality

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,9 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'accounts.User'
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -127,7 +131,21 @@ STATICFILES_DIRS = [
     'hotel_management/static'
 ]
 
+
+
+# media files configuration
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
